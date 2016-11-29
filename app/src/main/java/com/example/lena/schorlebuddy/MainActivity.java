2fa0f5e8409af7ed862e25e10267a9dbdd37b5d6
Daniela <leitzdaniela@gmail.com>
2016-11-29 16:11:42 +0100

    commit in new branch

--- app/src/main/java/com/example/lena/schorlebuddy/MainActivity.java
+++ app/src/main/java/com/example/lena/schorlebuddy/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lena.schorlebuddy;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,16 +15,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+        TextView textview;
+    static final int PROFILE_PIC_REQUEST = 1;
+    static final int DIARY_PIC_REQUEST = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Date d = new Date();
+        CharSequence s  = DateFormat.format("d MMMM yyyy ", d.getTime());
+        textview = (TextView)findViewById(R.id.date);
+        textview.setText(s);
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +56,50 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    public void onImageButtonClick(View view)
+    {
+        //Toast.makeText(this, "clicked button "+getResources().getResourceName(view.getId()), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "clicked ImageButton "+ String.valueOf(view.getTag()), Toast.LENGTH_SHORT).show();
+    }
+
+    public void onProfileButtonClick(View view)
+    {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, PROFILE_PIC_REQUEST);
+        //setResult(PROFILE_PIC_REQUEST, cameraIntent);
+    }
+    public void onCameraButtonClick(View view)
+    {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent,DIARY_PIC_REQUEST);
+    }
+
+    public void onMapsButtonClick(View view)
+    {
+        String uri = "http://maps.google.com/maps";
+        Intent mapsIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(uri));
+        startActivity(mapsIntent);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case PROFILE_PIC_REQUEST:
+                if (data.hasExtra("data")) {
+                    Bitmap image = (Bitmap) data.getExtras().get("data");
+                    ImageButton imagebtn = (ImageButton) findViewById(R.id.imgBtn_profile);
+                    imagebtn.setImageBitmap(image);
+//              BitmapDrawable drawable_bitmap = new BitmapDrawable(getResources(), image);
+//              imagebtn.setBackground(drawable_bitmap);
+                }
+                break;
+            case DIARY_PIC_REQUEST:
+                //bild an diary senden
+                break;
+            default:break;
+        }
+
+
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
